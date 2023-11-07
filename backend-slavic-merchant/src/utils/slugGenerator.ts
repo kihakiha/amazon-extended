78addcc822698aa5bb1d9959c07d0d5867d55ab7
Grigoriy Rusin <rusingrisha@gmail.com>
2023-11-07 13:35:00 +0500

    Utils

---
+++ backend-slavic-merchant/src/utils/slugGenerator.ts
@@ -0,0 +1,16 @@
+export function slugGenerator(text: string): string {
+  const translitMap = {
+  'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k',
+  'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+  'ш': 'sh', 'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya', ' ': '-', ',': '', '.': ''
+  };
+  
+  const slug = text.toLowerCase()
+  .replace(/./g, function(char) {
+  return translitMap[char] || char;
+  })
+  .replace(/[^a-z0-9-]/g, '')
+  .replace(/-+/g, '-');
+  
+  return slug;
+}
